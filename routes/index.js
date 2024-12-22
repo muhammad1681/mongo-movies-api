ef3cff687ef055762a879f816461a292a896375b
Thomas Rogers <Mr.Thomas.Rogers@Proton.me>
2024-12-22 23:45:17 +0000

    fix small issues

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const Movie = require("../models/movieModel");
 
 router.get('/', function(req, res, next) {
   res.json("Hello, World! test");
 });
 
-// router.get("/get-all-movies", function(res, next){
-  
-// });
+router.get("/get-all-movies", async function(req, res, next){
+  try {
+    const movies = await Movie.find();
+    res.status(200).json(movies);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
+
+router.post("/add-movie", async (req, res) => {
+  const { title, director, releaseYear, genre } = req.body;
+
+  try {
+    const movie = new Movie({ title, director, releaseYear, genre });
+    await movie.save();
+    res.status(201).json(movie);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err);
+  }
+})
 
 module.exports = router;
