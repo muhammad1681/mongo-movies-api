d63cdbc61825c88649f43d0a844c1d3d9a863f68
Thomas Rogers <Mr.Thomas.Rogers@Proton.me>
2024-12-25 00:57:11 +0000

    update schema

--- models/movieModel.js
+++ models/movieModel.js
@@ -2,17 +2,20 @@ const mongoose = require('mongoose');
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const movieSchema = new mongoose.Schema({
-    title: { type: String, required: true },
+    title: { type: String, required: true, unique: true },
     releaseYear: { type: Number, required: true },
+    rated: { type: String, required: false },
+    actors: { type: String, requred: true },
+    plot: {type: String, requred: false},
     director: { type: String, required: true },
     stars: {
-      type: Number,
-      required: true,
-      min: 1,
-      max: 5, 
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5, 
     },
-    review: { type: String, required: false },
-  });
+    posterUrl: { type: String, required: false },
+});
   
 
 movieSchema.plugin(AutoIncrement, { inc_field: 'id' });
