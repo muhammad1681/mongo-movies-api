5fc73f4f6d864d7f5b4ec85df8ccb350a970ddbe
muhammad1681 <muhammad@hule.vc>
2025-01-30 22:07:20 +0000

    App Update

--- app.js
+++ app.js
@@ -1,43 +1,48 @@
-require('dotenv').config();
-const createError = require('http-errors');
-const mongoose = require('mongoose');
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+require("dotenv").config();
+const createError = require("http-errors");
+const mongoose = require("mongoose");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const healthRouter = require('./routes/health');
+const indexRouter = require("./routes/index");
+const healthRouter = require("./routes/health");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/api', indexRouter);
-app.use('/api/health', healthRouter);
+app.use("/api", indexRouter);
+app.use("/api/health", healthRouter);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log("MongoDB not Connected: " + err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "sample_mflix",
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("MongoDB not Connected: " + err));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   res.status(err.status || 500);
   res.json({ error: err.message });
 });
 
-const port = process.env.PORT ?? 5000;
+const port = process.env.PORT ?? 4001;
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const Movie = require("../models/movieModel");
 
-router.get("/get-all-movies", async function(req, res, next){
+router.get("/get-all-movies", async function (req, res, next) {
   try {
-    const movies = await Movie.find();
+    const movies = await Movie.find()
+      .limit(req.query.limit)
+      .skip(req.query.skip);
+    // const movies = await Movie.find();
     res.status(200).json(movies);
   } catch (err) {
     console.log(err);
@@ -13,56 +16,55 @@ router.get("/get-all-movies", async function(req, res, next){
 });
 
 router.get("/get-by-id", async (req, res) => {
-  const id = req.headers['id'];
-  if(!id) res.status(400).json({error: "missing header of 'id'"});
-  
-  try{
-    const movie = await Movie.findOne({id: id})
+  const id = req.headers["id"];
+  if (!id) res.status(400).json({ error: "missing header of 'id'" });
 
-    if(movie === null){
-      res.status(404).json({error: `No movies found with id of ${id}`});
+  try {
+    const movie = await Movie.findOne({ id: id });
+
+    if (movie === null) {
+      res.status(404).json({ error: `No movies found with id of ${id}` });
       return;
     }
 
     res.status(200).json(movie);
-  }catch(err){
+  } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
 
-router.get("/search", async function(req, res, next){
-  const search = req.headers['search-value'];
-  if(!search){
-    res.status(400).json({error: "Missing header of 'search-value'"});
+router.get("/search", async function (req, res, next) {
+  const search = req.headers["search-value"];
+  if (!search) {
+    res.status(400).json({ error: "Missing header of 'search-value'" });
     return;
   }
 
   console.log(search);
-  
-  try{
+
+  try {
     const searchedMovies = await Movie.find({
-      title: { $regex: search, $options: 'i' }
+      title: { $regex: search, $options: "i" },
     });
 
     res.status(200).json(searchedMovies);
-  }catch(err){
+  } catch (err) {
     console.log(err);
     res.status(400).send(err);
   }
 });
 
-
 router.post("/add-movie", async (req, res) => {
-  const { 
-    title, 
-    releaseYear, 
-    rated = null, 
-    actors, 
-    plot = null, 
-    director, 
-    stars, 
-    posterUrl = null 
+  const {
+    title,
+    releaseYear,
+    rated = null,
+    actors,
+    plot = null,
+    director,
+    stars,
+    posterUrl = null,
   } = req.body;
 
   try {
@@ -74,7 +76,7 @@ router.post("/add-movie", async (req, res) => {
       plot,
       director,
       stars,
-      posterUrl
+      posterUrl,
     });
 
     await movie.save();
@@ -85,27 +87,30 @@ router.post("/add-movie", async (req, res) => {
   }
 });
 
-
 router.delete("/delete", async (req, res) => {
-  const id = req.headers['id'];
-  if(!id) {
-    res.status(400).json({error: "missing header of id. please provide a movie id"});
+  const id = req.headers["id"];
+  if (!id) {
+    res
+      .status(400)
+      .json({ error: "missing header of id. please provide a movie id" });
     return;
   }
 
-  try{
-    const movieToDel = await Movie.findOne({id:id});
-    if(movieToDel === null){
-      res.status(404).send({error: `Can not delete, no movie found with id '${id}'`});
+  try {
+    const movieToDel = await Movie.findOne({ id: id });
+    if (movieToDel === null) {
+      res
+        .status(404)
+        .send({ error: `Can not delete, no movie found with id '${id}'` });
       return;
-    } 
+    }
 
-    const result = await Movie.deleteOne({id:id});
+    const result = await Movie.deleteOne({ id: id });
     console.log(result);
-    res.status(200).json({message: "movie deleted"});
-  }catch(err){
+    res.status(200).json({ message: "movie deleted" });
+  } catch (err) {
     console.log(err);
-    res.status(500).json({error: err});
+    res.status(500).json({ error: err });
   }
 });
 
@@ -113,30 +118,34 @@ router.patch("/update/:id", async (req, res) => {
   const id = req.params.id;
   // const {title, releaseYear, director, stars, review} = req.body;
 
-  if(!id || !req.body){
-    return res.status(400).json({error: "Missing param, please provide an id"});
+  if (!id || !req.body) {
+    return res
+      .status(400)
+      .json({ error: "Missing param, please provide an id" });
   }
 
-  const movieToUpdate = await Movie.findOne({id:id});
-  if(!movieToUpdate){
-    res.status(404).json({error: `Can not update, no movie found with id '${id}`});
+  const movieToUpdate = await Movie.findOne({ id: id });
+  if (!movieToUpdate) {
+    res
+      .status(404)
+      .json({ error: `Can not update, no movie found with id '${id}` });
     return;
   }
 
   const movieId = movieToUpdate._id;
 
-  try{
+  try {
     const updatedMovie = await Movie.findByIdAndUpdate(movieId, req.body, {
       new: true,
-      runValidators: true
+      runValidators: true,
     });
 
-    if(!updatedMovie){
+    if (!updatedMovie) {
       return res.status(404).json({ message: "Movie not found" });
     }
 
     res.status(200).json(updatedMovie);
-  }catch(err){
+  } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Error updating movie" });
   }
